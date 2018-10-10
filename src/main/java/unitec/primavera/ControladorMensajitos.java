1aa3e454022528ab83c778146db54cde1fbe9b6b
T-107 <T-107@PC193>
2018-10-09 21:41:32 -0500

    09-10

--- src/main/java/unitec/primavera/ControladorMensajitos.java
+++ src/main/java/unitec/primavera/ControladorMensajitos.java
@@ -5,10 +5,16 @@
  */
 package unitec.primavera;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,4 +51,29 @@ public class ControladorMensajitos {
     public Mensaje buscasPorId(@PathVariable String id){
         return mensa.findById(id).get();
     }
+    
+    //vamos a guardar que siempre usa el post
+    //consumes le indica que vamos a usar un json
+    @PostMapping(path="/mensaje", consumes="application/json")
+    //le metemos el parametro que viene del body y le indicamos que viene en json
+    public Status guardar(@RequestBody String json)throws Exception{
+        //recibimos al json 
+        //mapeado que convierte de un formato a otro mapeo
+        
+        //creamos el objeto del object mapper
+        ObjectMapper maper=new ObjectMapper();
+        //de la clase mensaje creamos un objeto que va a teneer el metodo del 
+        //mapper readvalue y le metemos parametros que es un json de la clase mensaje
+        Mensaje mensajito= maper.readValue(json, Mensaje.class);
+        System.out.println(mensajito);
+        Status estatus= new Status();
+        estatus.setSuccess(true);
+        estatus.setMensaje("mensajito guardado con exito");
+        return estatus;
+        
+    }
+    /*
+    @PutMapping (path="/actualiza", consumes="application/json")
+    public
+    */
 }
